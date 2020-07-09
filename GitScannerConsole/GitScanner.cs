ab1eda4e127d5d92d465e29bc050c96f053580e6
Michel <michelschep@gmail.com>
2020-07-09 15:49:58 +0200

    Wip

---
+++ GitScannerConsole/GitScanner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace GitScannerConsole
+{
+    class GitScanner
+    {
+        public void Scan(string root)
+        {
+            var rootFolder = new DirectoryInfo(root);
+
+            Scan(rootFolder);
+        }
+
+        void Scan(DirectoryInfo rootFolder)
+        {
+            // TODO For some reason when a path is too long the IsGitRepo gives an exception
+            // For now just ignore these folders. 
+            if (PathIsTooLong(rootFolder))
+                return;
+
+            if (IsGitRepo(rootFolder))
+            {
+                CheckChangesInGitRepo(rootFolder);
+                return;
+            }
+
+            foreach (var directoryInfo in rootFolder.GetDirectories())
+            {
+                Scan(directoryInfo);
+            }
+        }
+
+        bool PathIsTooLong(DirectoryInfo rootFolder)
+        {
+            // Hack...
+            try
+            {
+                var fullName = rootFolder.FullName;
+                rootFolder.GetDirectories(".git");
+                return false;
+            }
+            catch (PathTooLongException)
+            {
+                return true;
+            }
+
+        }
+
+        bool IsGitRepo(DirectoryInfo rootFolder)
+        {
+            return rootFolder.GetDirectories(".git").Any();
+        }
+
+        void CheckChangesInGitRepo(DirectoryInfo rootFolder)
+        {
+            try
+            {
+                var repo = new Repository(rootFolder.FullName);
+                var options = new StatusOptions();
+                var repositoryStatus = repo.RetrieveStatus(options);
+
+                if (repositoryStatus.IsDirty)
+                    Console.WriteLine("Repo [{0} in {1}] is dirty", rootFolder.Name, rootFolder.Parent.FullName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Repo [{0}] cannot be read: [{1}]", rootFolder.Name, ex.Message);
+            }
+        }
+    }
+}
---
+++ GitScannerConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GitScannerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitScanner = new GitScanner();
+
+            var root = @"C:\data\projects-michel";
+
+            if (args.Length != 0)
+                root = args[0];
+
+            gitScanner.Scan(root);
+
+            Console.ReadLine();
+        }
+    }
+}
